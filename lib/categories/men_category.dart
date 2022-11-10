c08dc41a82c5bc26935aa11e2f03efd8518f5438
Ryoma Arisawa <fishing0831.au@gmail.com>
2022-11-10 11:23:31 +0900

    lesson17

--- lib/categories/men_category.dart
+++ lib/categories/men_category.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:multi_store_app/utilities/categ_list.dart';
 
-import '../minor_screens/subcategory_products.dart';
+import '../widgets/category_widgets.dart';
 
 class MenCategory extends StatelessWidget {
   const MenCategory({Key? key}) : super(key: key);
@@ -21,17 +21,7 @@ class MenCategory extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text(
-                      'Men',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.5,
-                      ),
-                    ),
-                  ),
+                  const CategoryHeaderLabel(headerLabel: 'Men'),
                   SizedBox(
                     height: MediaQuery.of(context).size.height * 0.68,
                     child: GridView.count(
@@ -40,28 +30,11 @@ class MenCategory extends StatelessWidget {
                       crossAxisCount: 3,
                       children: List.generate(
                         men.length,
-                        (index) => GestureDetector(
-                          onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => SubCategoryProducts(
-                                mainCategoryName: 'men',
-                                subCategoryName: men[index],
-                              ),
-                            ),
-                          ),
-                          child: Column(
-                            children: [
-                              SizedBox(
-                                height: 70,
-                                width: 70,
-                                child: Image(
-                                  image: AssetImage('images/men/men$index.jpg'),
-                                ),
-                              ),
-                              Text(men[index]),
-                            ],
-                          ),
+                        (index) => SubCategoryModel(
+                          mainCategoryName: 'men',
+                          subCategoryName: men[index],
+                          assetName: 'images/men/men$index.jpg',
+                          subCategoryLabel: men[index],
                         ),
                       ),
                     ),
@@ -70,56 +43,11 @@ class MenCategory extends StatelessWidget {
               ),
             ),
           ),
-          Positioned(
+          const Positioned(
             bottom: 0,
             right: 0,
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.8,
-              width: MediaQuery.of(context).size.width * 0.05,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 40),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.brown.withOpacity(0.2),
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: RotatedBox(
-                    quarterTurns: 3,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        const Text(
-                          ' << ',
-                          style: TextStyle(
-                            color: Colors.brown,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            letterSpacing: 10,
-                          ),
-                        ),
-                        Text(
-                          'men'.toUpperCase(),
-                          style: const TextStyle(
-                            color: Colors.brown,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            letterSpacing: 10,
-                          ),
-                        ),
-                        const Text(
-                          ' >> ',
-                          style: TextStyle(
-                            color: Colors.brown,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            letterSpacing: 10,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+            child: SliderBar(
+              mainCategoryName: 'men',
             ),
           ),
         ],
---
+++ lib/widgets/category_widgets.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+import '../minor_screens/subcategory_products.dart';
+
+class SliderBar extends StatelessWidget {
+  final String mainCategoryName;
+
+  const SliderBar({
+    Key? key,
+    required this.mainCategoryName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.8,
+      width: MediaQuery.of(context).size.width * 0.05,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 40),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.brown.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: RotatedBox(
+            quarterTurns: 3,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text(
+                  ' << ',
+                  style: TextStyle(
+                    color: Colors.brown,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 10,
+                  ),
+                ),
+                Text(
+                  mainCategoryName.toUpperCase(),
+                  style: const TextStyle(
+                    color: Colors.brown,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 10,
+                  ),
+                ),
+                const Text(
+                  ' >> ',
+                  style: TextStyle(
+                    color: Colors.brown,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 10,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SubCategoryModel extends StatelessWidget {
+  final String mainCategoryName;
+  final String subCategoryName;
+  final String assetName;
+  final String subCategoryLabel;
+
+  const SubCategoryModel({
+    Key? key,
+    required this.mainCategoryName,
+    required this.subCategoryName,
+    required this.assetName,
+    required this.subCategoryLabel,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SubCategoryProducts(
+            mainCategoryName: mainCategoryName,
+            subCategoryName: subCategoryName,
+          ),
+        ),
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 70,
+            width: 70,
+            child: Image(
+              image: AssetImage(assetName),
+            ),
+          ),
+          Text(subCategoryName),
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryHeaderLabel extends StatelessWidget {
+  final String headerLabel;
+
+  const CategoryHeaderLabel({
+    Key? key,
+    required this.headerLabel,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Text(
+        headerLabel,
+        style: const TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1.5,
+        ),
+      ),
+    );
+  }
+}
