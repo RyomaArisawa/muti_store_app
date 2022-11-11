caacc147176c4c7c29c57e76aba7d53d47de39ef
Ryoma Arisawa <fishing0831.au@gmail.com>
2022-11-11 11:52:26 +0900

    lesson22

--- lib/main_screens/profile.dart
+++ lib/main_screens/profile.dart
@@ -15,13 +15,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            centerTitle: true,
             elevation: 0,
             backgroundColor: Colors.white,
             expandedHeight: 140,
             flexibleSpace: LayoutBuilder(
               builder: (context, constrains) {
                 return FlexibleSpaceBar(
+                  centerTitle: true,
                   title: AnimatedOpacity(
                     duration: const Duration(milliseconds: 200),
                     opacity: constrains.biggest.height <= 120 ? 1 : 0,
@@ -129,9 +129,170 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     ],
                   ),
                 ),
+                const SizedBox(
+                  height: 150,
+                  child: Image(
+                    image: AssetImage('images/inapp/logo.jpg'),
+                  ),
+                ),
+                const ProfileHeaderLabel(
+                  headerLabel: 'Account Info.',
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 250,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      children: const [
+                        RepeatedListTile(
+                          title: 'Email Address',
+                          subtitle: 'example@email.com',
+                          iconData: Icons.email,
+                        ),
+                        YellowDivider(),
+                        RepeatedListTile(
+                          title: 'Phone Number',
+                          subtitle: '+11111',
+                          iconData: Icons.phone,
+                        ),
+                        YellowDivider(),
+                        RepeatedListTile(
+                          title: 'Address',
+                          subtitle: 'example:140 - st - New Gersy',
+                          iconData: Icons.location_pin,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const ProfileHeaderLabel(
+                  headerLabel: 'Account Settings',
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 210,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      children: [
+                        RepeatedListTile(
+                          title: 'Edit Profile',
+                          iconData: Icons.edit,
+                          onTap: () {},
+                        ),
+                        YellowDivider(),
+                        RepeatedListTile(
+                          title: 'Change Password',
+                          iconData: Icons.lock,
+                          onTap: () {},
+                        ),
+                        YellowDivider(),
+                        RepeatedListTile(
+                          title: 'Log Out',
+                          iconData: Icons.logout,
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           )
+        ],
+      ),
+    );
+  }
+}
+
+class YellowDivider extends StatelessWidget {
+  const YellowDivider({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      child: Divider(
+        color: Colors.yellow,
+        thickness: 1,
+      ),
+    );
+  }
+}
+
+class RepeatedListTile extends StatelessWidget {
+  final String title;
+  final IconData iconData;
+  final String? subtitle;
+  final VoidCallback? onTap;
+
+  const RepeatedListTile({Key? key, required this.title, required this.iconData, this.subtitle, this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: ListTile(
+        title: Text(title),
+        subtitle: subtitle != null ? Text(subtitle!) : null,
+        leading: Icon(iconData),
+      ),
+    );
+  }
+}
+
+class ProfileHeaderLabel extends StatelessWidget {
+  final String headerLabel;
+
+  const ProfileHeaderLabel({
+    Key? key,
+    required this.headerLabel,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 40,
+            width: 50,
+            child: Divider(
+              color: Colors.grey,
+              thickness: 1,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              headerLabel,
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+            width: 50,
+            child: Divider(
+              color: Colors.grey,
+              thickness: 1,
+            ),
+          ),
         ],
       ),
     );
